ac29db704a038d22072347fa95730e65371b612b
Ced1 <cedrickme@gmail.com>
2022-10-19 15:12:19 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computing the value of an integer
+ * @n: where the value is stored
+ *
+ * Return: the integer value
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+
+		return (n);
+	else
+		return (-n);
+
+}
