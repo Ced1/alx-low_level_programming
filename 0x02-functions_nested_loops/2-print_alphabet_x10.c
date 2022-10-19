b7c5a933c5237aea9a772beaaad91c1ef27c985c
Ced1 <cedrickme@gmail.com>
2022-10-19 13:43:42 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alph;
+
+	while (count++ <= 9)
+
+		for (alph = 'a'; alph <= 'z'; alph++)
+
+			_putchar(alph);
+
+	_putchar('\n');
+}
