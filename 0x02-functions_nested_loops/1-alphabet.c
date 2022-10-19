7a53db8b01580e394dcbfb7bdf89f236404bd814
Ced1 <cedrickme@gmail.com>
2022-10-19 13:09:03 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alp
+
+		for (alp = 'a'; alp <= 'z'; alp++)
+			_putchat(alp);
+
+	_putchar('\n');
+
+	return (0);
+}
